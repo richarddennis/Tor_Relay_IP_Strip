376c974500626144a328afaaba70fdceade82233
Richard Dennis <richard@dragoninfosec.com>
2017-09-13 10:36:56 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -5,12 +5,3 @@
 apt-get install -y git
 git clone https://github.com/richarddennis/Tor_Relay_IP_Strip.git
 ```
-
-Code
-
-Donwloads consensus from Tor servers
-Comapres to a list of nodes we are controlling to ensure they are still in the consensus
-Fromats the Consensus for Json
-
-TODO 
-Geolocate
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-file_to_write = open("test.json","rw+") 
-file_to_open = open("IPs.txt","rw") 
+#file_to_write = open("test.json","rw+") 
+#file_to_open = open("IPs.txt","r") 
 
-with open(file_to_open ) as f:
-    content = f.readlines()
-    print content
-    print "1"
+with open("IPs.txt","r") as f, open("Json_IPs.json", "w+") as w:
+	for line in f:
+		print line
+		w.write("{ 'ip':'")
+		w.write(line.rstrip())
+		w.write("' }")
+		w.write("\n")
