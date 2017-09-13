454d2dcfeb5fd2ba7362bef2983c9ed01b24ab83
Richard Dennis <richard@dragoninfosec.com>
2017-09-13 09:23:28 +0100

    Add files via upload

--- Richard.py
+++ Richard.py
@@ -11,6 +11,25 @@ for line in L:
     elif ListLine[0] == "dir-source" and ListLine[-4].split(".")[0].isalpha() == False:
         LineR.append(ListLine[-3] + "\n")
         LineR.append(ListLine[-4] + "\n")
-file = open("IPs.txt","w")
-file.writelines(LineR)
-file.close()
+
+readfile = open("ListIPs.txt","r")
+IPExist = []
+IPNoExist = []
+for line in readfile:
+    if line in LineR:
+        IPExist.append(line)
+    else:
+        IPNoExist.append(line)
+readfile.close()
+
+Newfile = open("AllIPs.txt","w")
+Newfile.writelines(LineR)
+Newfile.close()
+
+Newfile = open("ExistIPs.txt","w")
+Newfile.writelines(IPExist)
+Newfile.close()
+
+Newfile = open("NoExistIPs.txt","w")
+Newfile.writelines(IPNoExist)
+Newfile.close()
