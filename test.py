9d6a4c451ed0252059588d814cd577fa4af7a407
Richard Dennis <richard@dragoninfosec.com>
2017-09-13 10:05:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-file_to_write = open(“test.json”,”rw”) 
-file_to_open = open(“ip.txt”,”rw”) 
+file_to_write = open("test.json","rw") 
+file_to_open = open("ip.txt","rw") 
 
 with open(file_to_open ) as f:
     content = f.readlines()
